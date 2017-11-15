f9f66ea0c212027406046d01ecc126ad973118e0
Yeji Lee <yeji080808@gmail.com>
2017-11-14 23:16:38 -0800

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="Yeji")
 b2 = Button(window, text="60162157")
+b3 = Button(window, text="0808")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
